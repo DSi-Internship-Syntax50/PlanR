125aa9b3a87db59a0ff93e8a8d2eaac399cb56e8
Rafy Bhuiyan <rafybhuiyan23@gmail.com>
2026-04-12 15:03:15 +0600

    Update: Role base

--- src/main/java/com/example/PlanR/controller/AuthController.java
+++ src/main/java/com/example/PlanR/controller/AuthController.java
@@ -1,23 +1,12 @@
 package com.example.PlanR.controller;
-
-import com.example.PlanR.model.User;
-import com.example.PlanR.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
 
 @Controller
 public class AuthController {
-
-    @Autowired
-    private UserRepository userRepository;
-
     @GetMapping("/login")
     public String login() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
